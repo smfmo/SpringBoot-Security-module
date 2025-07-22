32a57c709a48249db69b2b8e6b4f82744ecd2924
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-07-22 13:58:56 -0300

    Implementação dos metodos GET e POST do controller para salvar e buscar todos os Grupos, e no service a criação da lógica de negócio.

--- src/main/java/io/github/smfmo/sbootSecurity/api/GrupoController.java
+++ src/main/java/io/github/smfmo/sbootSecurity/api/GrupoController.java
@@ -1,15 +1,33 @@
 package io.github.smfmo.sbootSecurity.api;
 
+import io.github.smfmo.sbootSecurity.domain.entity.Grupo;
 import io.github.smfmo.sbootSecurity.service.GrupoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/grupos")
 @RequiredArgsConstructor
 public class GrupoController {
 
-        private final GrupoService service;
+    private final GrupoService service;
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Grupo>> findAll() {
+        List<Grupo> grupos = service.findAll();
+        return ResponseEntity.ok(grupos);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> save(@RequestBody Grupo grupo) {
+        service.save(grupo);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/io/github/smfmo/sbootSecurity/service/GrupoService.java
+++ src/main/java/io/github/smfmo/sbootSecurity/service/GrupoService.java
@@ -1,8 +1,12 @@
 package io.github.smfmo.sbootSecurity.service;
 
+import io.github.smfmo.sbootSecurity.domain.entity.Grupo;
 import io.github.smfmo.sbootSecurity.infra.repository.GrupoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +14,13 @@ public class GrupoService {
 
     private final GrupoRepository repository;
 
+    public List<Grupo> findAll(){
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void save(Grupo grupo) {
+        repository.save(grupo);
+    }
 
 }
